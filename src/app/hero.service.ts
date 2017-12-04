43acd57092328c069430cb25159e3c477390f6c2
Lluis Toyos <toyos@winxp.dev.sgti.nl>
2017-12-04 14:18:58 +0100

    7. Routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,7 +12,12 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
-     this.messageService.add('HeroService: fetched heroes');
-     return of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
